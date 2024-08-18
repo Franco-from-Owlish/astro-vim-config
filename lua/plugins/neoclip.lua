07e7b6aab7e66e02261f29de48db25e9953d734c
Franco Grobler <franco@grobler.fyi>
2024-08-18 19:12:13 +0200

    Add telescope extensions

---
+++ lua/plugins/neoclip.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+require {
+  "AckslD/nvim-neoclip.lua",
+  lazy = true,
+  dependencies = {
+    { "nvim-telescope/telescope.nvim" },
+  },
+  config = function() require("neoclip").setup() end,
+}
