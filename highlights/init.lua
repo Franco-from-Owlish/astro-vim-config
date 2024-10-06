3bd6093bd22854de0f0c3364765dcb556488af48
Franco Grobler <franco@grobler.fyi>
2024-10-06 21:12:49 +0200

    Add user plugin

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,3 @@
 return {
-  Normal = { bg = "#000000" }
+  Normal = { bg = "#000000" },
 }
---
+++ lua/user/init.lua
@@ -0,0 +1,13 @@
+return {
+  options = {
+    opt = {
+      spell = true,
+      spelllang = "en_uk",
+    },
+  },
+  lsp = {
+    formatting = {
+      format_on_save = false, -- Disable format on save
+    },
+  },
+}
